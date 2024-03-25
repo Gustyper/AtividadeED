cf37743a0aee3c531d5ac2d970a025bbd71c1ea6
GUSTAVO BIANCHI DA SILVA <B51098@fgv.edu.br>
2024-03-25 08:02:38 -0300

    Corrigindo questao 1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
+#include <math.h> 
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 
-int iPotencia(int, int);
+int iPotencia(int&, int);
 
 int main()
 {
-    cout << iPotencia(6, 3);
+    int x = 2;
+    cout << iPotencia(x, 5);
     return 0;
 }
 
-int iPotencia(int numero, int power)
+int iPotencia(int& numero, int power)
 {
+    int resultado = numero;
+    
     for (int i = 1; i < power; i++)
     {
-        numero *= numero;
+        resultado *= numero;
     }
     
-    return numero;
+    return resultado;
 }
+
+// Notação húngara e referência
+// &atividade
