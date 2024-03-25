12aef1dd22d8251a9831f892fdef1ca6d6648bfc
GUSTAVO BIANCHI DA SILVA <B51098@fgv.edu.br>
2024-03-25 07:55:25 -0300

    Atividade 1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@ using std::cout;
 using std::endl;
 using std::string;
 
+int iPotencia(int, int);
+
 int main()
 {
+    cout << iPotencia(6, 3);
     return 0;
+}
+
+int iPotencia(int numero, int power)
+{
+    for (int i = 1; i < power; i++)
+    {
+        numero *= numero;
+    }
+    
+    return numero;
 }
